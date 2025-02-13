3a0737d51f5d9b6ca54c8e53a0bbcce6968d596e
Sovietianqi <130513099+Sovietianqi@users.noreply.github.com>
2025-02-13 18:16:48 +0800

    Add files via upload

---
+++ Lenin/dllmain.cpp
@@ -0,0 +1,23 @@
+﻿#include <Windows.h>
+#include "Stalin.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+DEFINE_HOOK(7CD810, EntryPoint, 9)
+{
+    return 0;
+}
